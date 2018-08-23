06da52d1ad5a2c343671222be9a715f7475048e7
Ed Guild <ed@vizzie.com>
2018-08-22 23:16:22 -0400

    Update ch-release-ml.sh

--- ch-release-ml.sh
+++ ch-release-ml.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Set up Missing Link for release
 
+# Pull the latest software release
+sh /usr/bin/ch-get-latest-software.sh
+
 # Set the hostname to MissingLink-XXXX
 sh /usr/bin/ch-set-hostname.sh
 
