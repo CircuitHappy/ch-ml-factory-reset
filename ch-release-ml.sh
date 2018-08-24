76f1fde8b3e241840152dc41c31ed38b61a65f62
Ed Guild <ed@vizzie.com>
2018-08-23 21:24:36 -0400

    Update ch-release-ml.sh

--- ch-release-ml.sh
+++ ch-release-ml.sh
@@ -7,6 +7,9 @@ sh /usr/bin/ch-get-latest-software.sh
 # Set the hostname to MissingLink-XXXX
 sh /usr/bin/ch-set-hostname.sh
 
+# Reset the WPA_Supplicant.conf
+cp /ch/defaults/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/
+
 # Disable SSH
 systemctl stop ssh
 systemctl disable ssh
