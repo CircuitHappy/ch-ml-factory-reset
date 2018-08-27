bc3c247a3a2a5fbf707c59d9f07dfe6251b161bb
Ed Guild <ed@vizzie.com>
2018-08-27 10:22:40 -0400

    Update ch-factory-reset.sh

--- ch-factory-reset.sh
+++ ch-factory-reset.sh
@@ -2,4 +2,4 @@
 # Reset Missing Link to Factory Defaults
 
 # Reset the WPA_Supplicant.conf
-cp /ch/defaults/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/
+/bin/cp /ch/defaults/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/
